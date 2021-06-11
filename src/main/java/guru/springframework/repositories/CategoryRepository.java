6a6acdb1759f06f5b51c0a6c4456d84d7aefc032
Dmitri Zakharov <dmitri.zakharov@desjardins.com>
2021-06-10 21:20:17 -0400

    s08-141 Spring Data JPA Query Methods

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -3,8 +3,12 @@ package guru.springframework.repositories;
 import guru.springframework.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 /**
  * Created by developer on 2021-06-09.
  */
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
